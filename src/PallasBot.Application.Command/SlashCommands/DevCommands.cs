cc7c5a3478e25b636d293d08b8fcba4ee6f7fe2b
alisa <alisa@alisaqaq.moe>
2025-01-19 20:31:37 +0800

    feat: add dev command that throw exception

---
+++ src/PallasBot.Application.Command/SlashCommands/DevCommands.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Interactions;
+using PallasBot.Domain.Attributes;
+
+namespace PallasBot.Application.Command.SlashCommands;
+
+[DevOnly]
+[RequireOwner]
+public class DevCommands : InteractionModuleBase
+{
+    [SlashCommand("throw-error", "Throws an error")]
+    public async Task ThrowError()
+    {
+        await RespondAsync("Will throw an error", ephemeral: true);
+        throw new InvalidOperationException("This is a test error");
+    }
+}
