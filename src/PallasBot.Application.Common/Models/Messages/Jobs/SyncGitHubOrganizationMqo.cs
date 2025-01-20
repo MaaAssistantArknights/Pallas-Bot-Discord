739866b09c9ff1a364a6975bed87b0d8944e1466
alisa <alisa@alisaqaq.moe>
2025-01-20 18:07:57 +0800

    chore: update record format

--- src/PallasBot.Application.Common/Models/Messages/Jobs/SyncGitHubOrganizationMqo.cs
+++ src/PallasBot.Application.Common/Models/Messages/Jobs/SyncGitHubOrganizationMqo.cs
@@ -4,4 +4,10 @@ public record SyncGitHubOrganizationMqo
 {
     public required bool SyncMembers { get; set; }
     public required string[] Repositories { get; set; }
+
+    public override string ToString()
+    {
+        var repositoriesFormatted = string.Join(", ", Repositories);
+        return $"SyncGitHubOrganizationMqo {{ SyncMembers = {SyncMembers}, Repositories = [{repositoriesFormatted}] }}";
+    }
 }
